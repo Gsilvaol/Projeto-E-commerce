274cc9f8fbb3c74448321d59578f09468415c05b
Gustavo <gustavosilva1994_@hotmail.com>
2025-09-09 14:59:11 -0300

    endpoint produto

--- apps/api/src/produto/produto.controller.ts
+++ apps/api/src/produto/produto.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProdutoPrisma } from './produto.prisma';
 import { Produto } from '@gstore/core';
 
@@ -7,7 +7,12 @@ export class ProdutoController {
   constructor(readonly produtoPrisma: ProdutoPrisma) {}
 
   @Get()
-  async obterTodos(): Promise <Produto[]>{
-    return this.produtoPrisma.obter()
+  async obterTodos(): Promise<Produto[]> {
+    return this.produtoPrisma.obter();
+  }
+
+  @Get(':id')
+  async obterUm(@Param('id') id: string): Promise<Produto | null> {
+    return this.produtoPrisma.obterUm(+id);
   }
 }
--- apps/api/src/produto/produto.prisma.ts
+++ apps/api/src/produto/produto.prisma.ts
@@ -4,10 +4,15 @@ import { PrismaProvider } from 'src/db/prisma.provider';
 
 @Injectable()
 export class ProdutoPrisma {
-    constructor(readonly prisma:PrismaProvider){}
+  constructor(readonly prisma: PrismaProvider) {}
 
-    async obter(): Promise<Produto[]>{
-        const produtos = await this.prisma.produto.findMany()
-        return produtos as Produto[]
-    }
+  async obter(): Promise<Produto[]> {
+    const produtos = await this.prisma.produto.findMany();
+    return produtos as Produto[];
+  }
+
+  async obterUm(id: number): Promise<Produto | null> {
+    const produto = await this.prisma.produto.findUnique({ where: { id } });
+    return (produto as Produto) ?? null
+  }
 }
