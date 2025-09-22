d1a30113c26c8bdba2276fbbe4d39a798c5b647a
Gustavo <gustavosilva1994_@hotmail.com>
2025-09-22 14:52:43 -0300

    titulo produto

--- apps/web/src/app/(paginas)/produtos/[id]/page.tsx
+++ apps/web/src/app/(paginas)/produtos/[id]/page.tsx
@@ -1,18 +1,25 @@
-"use client"
+"use client";
 import { useEffect, useState, use } from "react";
+import TituloProduto from "@/components/produto/TituloProduto";
 
-export default function PaginaProduto(props:any){
-    const { id }: { id: string } = use (props.params)
+export default function PaginaProduto(props: any) {
+  const { id }: { id: string } = use(props.params);
 
-    const [produto, setProduto] = useState(null);
-      useEffect(() => {
-        const urlBase = process.env.NEXT_PUBLIC_API_URL;
-        fetch(`${urlBase}/produtos/${id}`)
-          .then((dados) => dados.json())
-          .then((dados) => setProduto(dados));
-      }, []);
+  const [produto, setProduto] = useState(null);
+  useEffect(() => {
+    const urlBase = process.env.NEXT_PUBLIC_API_URL;
+    fetch(`${urlBase}/produtos/${id}`)
+      .then((dados) => dados.json())
+      .then((dados) => setProduto(dados));
+  }, []);
 
-    return(
-        <div>{JSON.stringify(produto)}</div>
-    )
+  return produto ? (
+    <div className="flex flex-col gap-20 container py-10 ">
+      <div className="flex flex-col gap-10">
+        <TituloProduto produto={produto} />
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 }
