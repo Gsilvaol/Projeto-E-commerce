ff30e202fd7a27031eb9118e56ea2eed167fdd07
Gustavo <gustavosilva1994_@hotmail.com>
2025-09-26 15:45:45 -0300

    formatando erros

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FormataFilter } from './filters/formata.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new FormataFilter())
 
   app.enableCors({
     origin: 'http://localhost:3000', // permite só teu frontend
